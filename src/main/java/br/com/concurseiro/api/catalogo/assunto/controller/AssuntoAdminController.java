03e10f96d45fd97f0d107fa2b7b4e0de2bba3501
Mauricio <carmaunt@gmail.com>
2026-02-28 21:38:35 -0300

    refactor: organização por domínio + ajustes segurança e catálogo

--- src/main/java/br/com/concurseiro/api/catalogo/assunto/controller/AssuntoAdminController.java
+++ src/main/java/br/com/concurseiro/api/catalogo/assunto/controller/AssuntoAdminController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class AssuntoAdminController {
 
     private final AssuntoService service;
+    
 
     public AssuntoAdminController(AssuntoService service) {
         this.service = service;
@@ -23,6 +24,8 @@ public class AssuntoAdminController {
             @NotNull Long disciplinaId,
             @NotBlank @Size(max = 200) String nome
     ) {}
+
+    
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
